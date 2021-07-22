83c7de8ab3c8e18450ba4eb2922d60100f01e99b
test1 <jojun-yeong@localhost>
2021-07-22 17:15:02 +0900

    07/22

---
+++ src/self_prac.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class self_prac {
+	Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		
+		ArrayList list1 = new ArrayList();
+		ArrayList list2 = new ArrayList();
+		
+		
+		
+		list1.add(5);
+		list1.add(4);
+		list1.add(2);
+		list1.add(0);
+		list1.add(1);
+		list1.add(3);
+
+		list2.add(4);
+		list2.add(2);
+		list2.add(0);
+		
+		System.out.println(list1);
+		System.out.println(list2);
+	}
+
+}
