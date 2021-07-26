3adf77320ed8761f26a6b248ee361a747a0157a8
test1 <jojun-yeong@localhost>
2021-07-26 13:03:54 +0900

    save 07/26

--- src/Ex11_3.java
+++ src/Ex11_3.java
@@ -15,7 +15,6 @@ public class Ex11_3 {
 			String input = "((2+3)*1)+3"; //args[0];
 			Stack st = new Stack();
 			
-//				System.out.println(st.size());
 			try {
 				
 				for(int i=0; i<input.length(); i++) {
@@ -28,7 +27,8 @@ public class Ex11_3 {
 				}
 				
 				
-				if(st.size()==0) {
+				System.out.println(st.size());
+				if(st.size()==0) { //== st.siEmpty
 					System.out.println("괄호가 일치합니다.");
 				}
 				else System.out.println("괄호가 일치하지 않습니다..");
